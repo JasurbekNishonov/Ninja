5a0e7dbd853b17caccec0e9af764a35ecfc7f779
jasur <nishonovjasur24@gmail.com>
2023-09-29 00:46:09 +0500

    index qoshdin

---
+++ script.js
@@ -0,0 +1,120 @@
+let shareBtn = document.getElementById("shareBtn");
+let scrollDown = document.getElementById("scroll-down");
+let h1 = document.getElementById("h1");
+let section = document.getElementById("section");
+let submit = document.getElementById("submit-btn");
+
+/*   let submit = document.getElementById("submit-btn");
+  submit.addEventListener("click", function(){
+    shareBtn.classList.toggle("open");
+  }) */
+
+  shareBtn.addEventListener("click", function(){
+    shareBtn.classList.toggle("open");
+  });
+
+  $('#submit').on('click', function(){
+    $('.section__register .container .row').animate({
+        top: 400
+    })
+    },1000)
+
+
+
+//const letters = "########*********?????????///////@@@@@@@@";
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+ 
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 50);
+}
+
+
+
+
+window.addEventListener("DOMContentLoaded",() => {
+	const starRating = new StarRating("form");
+});
+
+class StarRating {
+	constructor(qs) {
+		this.ratings = [
+			{id: 1, name: "Terrible"},
+			{id: 2, name: "Bad"},
+			{id: 3, name: "OK"},
+			{id: 4, name: "Good"},
+			{id: 5, name: "Excellent"}
+		];
+		this.rating = null;
+		this.el = document.querySelector(qs);
+
+		this.init();
+	}
+	init() {
+		this.el?.addEventListener("change",this.updateRating.bind(this));
+
+		// stop Firefox from preserving form data between refreshes
+		try {
+			this.el?.reset();
+		} catch (err) {
+			console.error("Element isn’t a form.");
+		}
+	}
+	updateRating(e) {
+		// clear animation delays
+		Array.from(this.el.querySelectorAll(`[for*="rating"]`)).forEach(el => {
+			el.className = "rating__label";
+		});
+
+		const ratingObject = this.ratings.find(r => r.id === +e.target.value);
+		const prevRatingID = this.rating?.id || 0;
+
+		let delay = 0;
+		this.rating = ratingObject;
+		this.ratings.forEach(rating => {
+			const { id } = rating;
+
+			// add the delays
+			const ratingLabel = this.el.querySelector(`[for="rating-${id}"]`);
+
+			if (id > prevRatingID + 1 && id <= this.rating.id) {
+				++delay;
+				ratingLabel.classList.add(`rating__label--delay${delay}`);
+			}
+
+			// hide ratings to not read, show the one to read
+			const ratingTextEl = this.el.querySelector(`[data-rating="${id}"]`);
+
+			if (this.rating.id !== id)
+				ratingTextEl.setAttribute("hidden",true);
+			else
+				ratingTextEl.removeAttribute("hidden");
+		});
+	}
+}
+
+
+
